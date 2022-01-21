dca80a94352c28b33bf69de6dd45a22a24291af1
Larry Mei <88351375+lmeiucsd@users.noreply.github.com>
2022-01-20 16:10:30 -0800

    initial commit

--- src/pages/CreateItem/index.tsx
+++ src/pages/CreateItem/index.tsx
@@ -11,19 +11,22 @@ import './style.less';
 const CreateItem: React.FC = () => {
   // const history = useHistory();
   const { apiUrl } = useContext(GlobalContext);
-  const [name, setName] = useState('');
-
+  const [name, setName] = useState(''); // state for name
+  const descriptionState = useState('');
+  const priceState = useState('');
   return (
     <>
       <Navbar title="Create Item" />
       <div className="createItem">
         <p className="createItem-description">Add an item to the world’s greatest store</p>
         <TextBox inputType="text" value={name} label="name" onChange={(t) => setName(t)} />
-
+        <TextBox inputType="text" value={descriptionState[0]} label="description" onChange={(t) => descriptionState[1](t)} />
+        <TextBox inputType="text" value={priceState[0]} label="price" onChange={(t) => priceState[1](t)} />
         <Button
           text="Create Item"
           onClick={async () => {
-            await createItem(apiUrl, name, 'description', 0);
+            const priceNum = parseFloat(priceState[0]);
+            await createItem(apiUrl, name, descriptionState[0], priceNum);
             // history.push(pathLinks.home); // when we're done, go home to see the new item!
           }}
         />
